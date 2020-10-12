1a4dfab65d7685e18216f916a51340f8d4ef2383
Anna <anna61122@gmail.com>
2020-10-12 08:41:59 +0800

    add basic unit test

---
+++ OrderSystemTests1/Controllers/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderSystem.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace OrderSystem.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
